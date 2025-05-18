df8b4b8db4c9e184746e7dc80dca7084aa230ced
kisernl <noah.kiser@gmail.com>
2025-05-18 15:42:04 -0500

    debug premium placeholder

--- src/components/OptionsCalculator.tsx
+++ src/components/OptionsCalculator.tsx
@@ -518,7 +518,8 @@ const OptionsCalculator: React.FC = () => {
                     type="number"
                     min="0"
                     step="0.01"
-                    value={getCurrentState().premium}
+                    value={getCurrentState().premium || ""}
+                    placeholder="0.00"  
                     onChange={handlePremiumChange}
                     className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                   />
